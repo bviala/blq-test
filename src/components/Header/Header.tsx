627600fe4dde17d6890db7c8c488dd627e2ec6c4
Baptiste Viala <bviala@palo-it.com>
2022-11-11 20:00:27 +1100

    basic theme

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import classes from './Header.module.css'
 const Header = () => {
     return (
     <header className={classes.header}>
-        <h1>
+        <h1 className='primary--text'>
             Brocollo & Co.
         </h1>
     </header>
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,7 +3,9 @@ import classes from './Main.module.css'
 const Main = () => {
     return (
         <main className={classes.main}>
-            TEST
+            <h1 className='primary--text'>A better way <br/>to enjoy every day.</h1>
+            <p>Be the first to know when we launch</p>
+            <button>Request an invite</button>
         </main>
     )
 }
