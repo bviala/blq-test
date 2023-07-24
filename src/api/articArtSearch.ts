2514ca1935d9de33eba198f8fb769b865f7dfaa5
bap <baptiste.viala@outlook.com>
2023-07-24 19:34:33 +1000

    PoC

---
+++ src/api/articArtSearch.ts
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+type ArtItem = {
+    id: string
+    image_id: string
+}
+
+type ArtSearchResponse = {
+    data: ArtItem[]
+}
+
+const useArticSearch = () => {
+    const [isPending, setIsPending] = useState(false)
+    const [result, setResult] = useState<ArtSearchResponse>()
+    const [error, setError] = useState('')
+
+    const ArticSearch = async (request: string) => {
+        setIsPending(true)
+        try {
+            const response = await fetch(`https://api.artic.edu/api/v1/artworks/search?q=${request}&limit=1&fields=image_id`)
+            if (!response.ok) {
+                const { errorMessage } = await response.json()
+                throw new Error(errorMessage)
+            }
+            const responseResult = await response.json()
+            setResult(responseResult)
+        } catch(error) {
+            let errorMessage = 'unknown error'
+            if (error instanceof Error) errorMessage = error.message
+            setError(errorMessage)
+        } finally {
+            setIsPending(false)
+        }
+    }
+    return { isPending, error, smkArtSearch: ArticSearch, result }
+}
+
+export default useArticSearch
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,10 @@
+import useArticSearch from '../../api/articArtSearch'
 import useSmkArtSearch from '../../api/smkArtSearch'
 import classes from './Main.module.css'
 import { useEffect } from 'react'
 
 const Main = () => {
-    const { isPending, error, smkArtSearch, result } = useSmkArtSearch()
+const { isPending, error, smkArtSearch, result } = useArticSearch()
 
     useEffect(() => {
         smkArtSearch('devil')
@@ -12,8 +13,11 @@ const Main = () => {
     return (
         <main className={classes.main}>
             {isPending ? (<p>pending</p>) : error ? (<p>{error}</p>) : (
-                result?.items.map(item => (
-                    <p>{item.id}</p>
+                result?.data.map((item, index) => (
+                    <div key={index}>
+                        {item.id}
+                        <img src={`https://www.artic.edu/iiif/2/${item.image_id}/full/843,/0/default.jpg`}/>
+                    </div>
                 ))
             )}
         </main>
