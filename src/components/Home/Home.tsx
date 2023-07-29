cc294b1f078021a0fad6c71e1704808d05a64038
bap <baptiste.viala@outlook.com>
2023-07-29 20:34:44 +1000

    adds react router

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,31 @@
+import useArticSearch from '../../api/articArtSearch'
+import { FormEvent, useState } from 'react'
+import classes from './Home.module.css'
+
+const App = () => {
+const { isPending, error, smkArtSearch, result } = useArticSearch()
+
+    const [searchQuery, setSearchQuery] = useState<string>('')
+
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault()
+        smkArtSearch(searchQuery)
+    }
+    
+    return (<>
+        <form className={classes['search-form']} onSubmit={handleSubmit}>
+            <input autoFocus onChange={event => setSearchQuery(event.target.value)} type='text' placeholder='eg. blue'/>
+            <input type='submit' value={isPending ? `looking for ${searchQuery}...` : 'search'} disabled={isPending} />
+        </form>
+
+        {error ? (<p className='error--text'>{error}</p>) : (
+            result.map((item, index) => (
+                <div className={classes.artwork} key={index}>
+                    <img alt={item.id} src={item.image_src}/>
+                </div>
+            ))
+        )}
+    </>)
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App/App'
+import Home from './components/Home/Home'
 import ReactModal from 'react-modal'
 import './index.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Home/>),
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Header/>
-    <App />
+    <main>
+      <RouterProvider router={router} />
+    </main>
     <Footer/>
   </React.StrictMode>
 )
